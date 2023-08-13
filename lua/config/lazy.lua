6f2b0a01964cf3a9dea71bc2d7e638b2eb69366d
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-08-13 21:54:58 +0200

    add lazy vim test extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
     { import = "plugins" },
   },
