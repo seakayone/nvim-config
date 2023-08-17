84fe669ee7cebd62a407f0d28286b7d1955ac8ca
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-08-17 22:42:17 +0200

    add debugging

--- lua/plugins/extras/lang/scala.lua
+++ lua/plugins/extras/lang/scala.lua
@@ -37,6 +37,28 @@ return {
         end,
         group = nvim_metals_group,
       })
+
+      -- Debug settings
+      local dap = require("dap")
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+          },
+        },
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test Target",
+          metals = {
+            runType = "testTarget",
+          },
+        },
+      }
     end,
   },
 }
